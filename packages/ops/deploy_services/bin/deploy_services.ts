e9ee833d0c97e6e821b3f0d01b8e4a15860ee064
Sushil Shinde <sushil.shinde@tech9.com>
2023-04-16 13:50:16 +0530

    add dynamodb stack

--- packages/ops/deploy_services/bin/deploy_services.ts
+++ packages/ops/deploy_services/bin/deploy_services.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { APIStack } from '../lib/deploy_services-stack';
+import {DBStack} from "../lib/dynamodb-stack";
 const { STAGE, CDK_DEFAULT_REGION, CDK_DEFAULT_ACCOUNT } = process.env;
 
 console.log(CDK_DEFAULT_REGION, CDK_DEFAULT_ACCOUNT)
@@ -10,4 +11,8 @@ const app = new cdk.App();
 
 new APIStack(app, 'DigitalBarcodeAPIStack',  {
   env: { region: CDK_DEFAULT_REGION, account: CDK_DEFAULT_ACCOUNT },
+});
+
+new DBStack(app, `${STAGE}-digital-barcode-db`, {
+  env: { region: CDK_DEFAULT_REGION, account: CDK_DEFAULT_ACCOUNT},
 });
